c437866b80b24541e9a89d8e8eb6ca3b06a8a112
Alex Miller <alex.k.miller@gmail.com>
2018-10-29 08:37:51 -0400

    No email on error

--- dhs_page_watcher.py
+++ dhs_page_watcher.py
@@ -94,11 +94,7 @@ def send_email(subject, message):
 
 
 def main():
-    try:
-        raw_content = fetch_data()
-    except Exception as e:
-        print("Encountered an error fetching data...")
-        send_email("DHS page fetch has failed", "<p>Error message: "+str(e)+"</p>")
+    raw_content = fetch_data()
     its_the_same, old_raw_content = data_is_the_same(raw_content)
     if not its_the_same:
         changes_old, changes_new = get_diff(raw_content_to_table(old_raw_content), raw_content_to_table(raw_content))
